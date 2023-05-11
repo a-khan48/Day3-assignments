882e69fb0d585493ac1aa4e55e74bc90f9296408
Ahmad Khan <ahmad.d.khan40@gmail.com>
2023-05-10 22:33:37 -0400

    final

--- D3/src/main/java/org/chatBot/chatBot.java
+++ D3/src/main/java/org/chatBot/chatBot.java
@@ -2,7 +2,12 @@ package org.chatBot;
 import java.util.Scanner;
 public class chatBot {
     public static void main(String[] args){
-
+            greeting();
+            name();
+            age();
+            count();
+            test();
+            System.out.println("Nice chatting with you, Goodbye!");
 
     }
     public static void greeting(){
@@ -12,39 +17,61 @@ public class chatBot {
     public static void name(){
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your name: ");
-        System.out.println("Can you re-type your name one more time to confirm?");
         String userName = scanner.nextLine();
-        System.out.println("Hello " + userName + ", Nice to meet you!");
+        System.out.println("Can you re-type your name one more time to confirm?");
+        String userName2 = scanner.nextLine();
+        System.out.println("Hello " + userName2 + ", Nice to meet you!");
     }
     public static void age(){
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Let's play a little game.");
-        System.out.println("I'm going to ask you a few questions, depending on your answer, I'll try and guess your age!");
-//        Questions
-        System.out.println("Do you remember a life before smartphones?");
-        String res1 = scanner.nextLine();
+        System.out.println("I'm going to try and guess your age, what is your birth year?");
 
-        if (res1.equalsIgnoreCase("no")){
-            System.out.println("I'm going to assume you are under the age of 18.");
-        } else {
-            System.out.println("Do you remember the beginning of the Afghanistan War?");
-            String res2 = scanner.nextLine();
+        int birthYear = scanner.nextInt();
+        scanner.nextLine();
+        int age = 2023 - birthYear;
+        System.out.println("I assume you are " + age + " years old.");
+        System.out.println("Was i correct?");
+        String answer = scanner.nextLine();
 
-            if (res2.equalsIgnoreCase("no")){
-                System.out.println("I am going to assume you are around the age of 18-24");
-            } else{
-                System.out.println("Do you remember the end of the Cold War?");
-                String res3 = scanner.nextLine();
+        while (answer.equalsIgnoreCase("no")) {
+            System.out.println("I'm sorry for getting it wrong.");
+            age = 2023 - birthYear - 1;
+            System.out.println("Your age should be " + age);
+            System.out.println("Was I correct this time?");
+            answer = scanner.nextLine();
+        }
+    }
+    public static int count(){
+        System.out.println("Give me a number and I will count to it.");
+        Scanner scanner = new Scanner(System.in);
+        int count = scanner.nextInt();
+        int i = 0;
+        for( i = 1; i < count; i++) {
+            System.out.println(i);
+        }
+        System.out.println(count);
+        return i;
+    }
+    public static void test() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Think fast! Here's a quiz!");
+        System.out.println("What programming language are we learning right now?");
 
-                if (res2.equalsIgnoreCase("no")) {
-                    System.out.println("I am going to assume you are around the age of 25-35");
-                }else {
-                    System.out.println("I am going to assume you are older than 35.");
-                }
-                public static void count(){
+        System.out.println("A. Java");
+        System.out.println("B. JavaScript");
+        System.out.println("C. HTML");
+        System.out.println("D. CSS");
+        System.out.println("Type in your answer(A or B or C or D or E) ");
+        String answer = scanner.nextLine();
 
-                }
+        while (true) {
+
+            if (answer.equalsIgnoreCase("A")) {
+                System.out.println("Good job! You got it right.");
+                break;
             }
+            System.out.println("Oh no! Try again!");
+            answer = scanner.nextLine();
         }
     }
 }
